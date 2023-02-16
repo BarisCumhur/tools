3713040952ebb88d2b5ef447188759b4e417bc28
Baris Cumhur <baris_cumhur@hotmail.com>
2023-02-16 23:12:35 +0300

    upodate

--- build/boost.py
+++ build/boost.py
@@ -4,10 +4,10 @@ import shutil
 import tools.os
 
 
-def do_install(deps_path, install_path):
+def do_install(src_path, install_path):
 
     old_cwd = os.getcwd()
-    os.chdir(f"{deps_path}/boost")
+    os.chdir(src_path)
     if not os.path.isfile("b2.{tools.os.EXE_EXT}"):
         tools.os.run(
             f'.{tools.os.PATH_SEP}bootstrap{tools.os.SHELL_EXT}',
