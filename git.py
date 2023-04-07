9df3379e22dee377c6d2eca3c2f6fd742d4bdbab
Baris Cumhur <baris_cumhur@hotmail.com>
2023-04-07 22:52:02 +0300

    update

--- git.py
+++ git.py
@@ -4,23 +4,18 @@ import os
 import tools.os
 
 
+def init_submodules(path):
+    old_cwd = os.getcwd()
+    os.chdir(path)
+    tools.os.run(f'git submodule init', "pull")
+    tools.os.run(f'git submodule update --recursive', "pull")
+    os.chdir(old_cwd)
+
+
 def prepare_submodule(src_path):
     old_cwd = os.getcwd()
     os.chdir(src_path)
-    tools.os.run(
-        f'git reset --hard',
-        "reset"
-    )
-    tools.os.run(
-        f'git pull',
-        "pull"
-    )
-    tools.os.run(
-        f'git submodule init',
-        "pull"
-    )
-    tools.os.run(
-        f'git submodule update --recursive',
-        "pull"
-    )
+    tools.os.run(f'git reset --hard', "reset")
     os.chdir(old_cwd)
+
+    init_submodules(src_path)
