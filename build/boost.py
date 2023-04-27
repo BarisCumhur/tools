86b13927294003dd67c8abebff04695b432d9500
Barış Cumhur <baris_cumhur@hotmail.com>
2023-04-27 10:10:10 +0300

    update

--- build/boost.py
+++ build/boost.py
@@ -11,7 +11,7 @@ def do_install(src_path, install_path, options=[]):
 
     old_cwd = os.getcwd()
     os.chdir(src_path)
-    if not os.path.isfile("b2.{tools.os.EXE_EXT}"):
+    if not os.path.isfile("b2{tools.os.EXE_EXT}"):
         tools.os.run(
             f'.{tools.os.PATH_SEP}bootstrap{tools.os.SHELL_EXT}',
             "bootstrap"
