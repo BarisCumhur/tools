7fa294ce5e80d664816f30da8e2a7ba0f4aced2c
Barış Cumhur <baris.cumhur@tai.com.tr>
2024-03-12 11:12:20 +0300

    cmake can be used with a wrapper

--- build/cmake.py
+++ build/cmake.py
@@ -4,11 +4,11 @@ import tools.os
 import tools.git
 
 
-def do_install(src_path, build_path, install_path, options):
+def do_install(src_path, build_path, install_path, options, wrapper=""):
     src_path = os.path.abspath(src_path)
     build_path = os.path.abspath(build_path)
     install_path = os.path.abspath(install_path)
-    
+
     if not os.path.isdir(build_path):
         os.makedirs(build_path)
 
@@ -16,26 +16,27 @@ def do_install(src_path, build_path, install_path, options):
     old_cwd = os.getcwd()
     os.chdir(build_path)
 
-    joined_options = ' '.join([
-        '-DCMAKE_DEBUG_POSTFIX=d',
-        f'-DCMAKE_INSTALL_PREFIX={install_path}'
-    ] + options)
-
-    tools.os.run(
-        f'cmake {src_path} {joined_options}',
-        "configuration"
+    joined_options = " ".join(
+        ["-DCMAKE_DEBUG_POSTFIX=d", f"-DCMAKE_INSTALL_PREFIX={install_path}"] + options
     )
+
+    tools.os.run(f"{wrapper} cmake {src_path} {joined_options}", "configuration")
     tools.os.run(
         f"cmake --build . --target install --config Debug --parallel {multiprocessing.cpu_count()}",
-        "debug install"
+        "debug install",
     )
     tools.os.run(
         f"cmake --build . --target install --config Release --parallel {multiprocessing.cpu_count()}",
-        "release install"
+        "release install",
     )
 
     os.chdir(old_cwd)
 
 
-def package_exists(name, compiler_id='GNU'):
-    return os.system(f'cmake --find-package -DNAME={name} -DCOMPILER_ID={compiler_id} -DLANGUAGE=CXX -DMODE=EXIST') == 0
+def package_exists(name, compiler_id="GNU"):
+    return (
+        os.system(
+            f"cmake --find-package -DNAME={name} -DCOMPILER_ID={compiler_id} -DLANGUAGE=CXX -DMODE=EXIST"
+        )
+        == 0
+    )
